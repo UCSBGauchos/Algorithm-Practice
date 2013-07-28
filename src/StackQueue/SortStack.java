a320f25f495c8a40f6d7a879e2b21489bc1c26c1
yangboufo <130469Uf>
2013-07-28 17:49:50 +0800

    Sort the stack

---
+++ src/StackQueue/SortStack.java
@@ -0,0 +1,29 @@
+package StackQueue;
+
+public class SortStack {
+	public Stack sort(Stack s){
+		Stack helper = new Stack();
+		while(s.getLength()!=0){
+			int tmp = s.pop().data;
+			while(helper.getLength()!=0 && helper.peek()>tmp){
+				s.push(helper.pop().data);
+			}
+			helper.push(tmp);
+		}
+		return helper;
+	}
+	
+	public static void main(String [] args){
+		Stack stack = new Stack();
+		stack.push(1);
+		stack.push(3);
+		stack.push(2);
+		stack.push(5);
+		stack.push(6);
+		SortStack sort = new SortStack();
+		Stack result = sort.sort(stack);
+		while(result.getLength()!=0){
+			System.out.println(result.pop().data);
+		}
+	}
+}
