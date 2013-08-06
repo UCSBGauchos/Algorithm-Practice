f2d2772a204d130f5e1848e2105e693bd50f7f3d
yangboufo <130469Uf>
2013-08-06 22:55:52 +0800

    Image process

--- src/Recurse/Image.java
+++ src/Recurse/Image.java
@@ -1,5 +1,21 @@
 package Recurse;
 
 public class Image {
-
+	enum Color{
+		Black, White, Red, Yellow, Green
+	}
+	
+	public boolean paintFill(Color [][] pixels, int x, int y, Color oldColor, Color newColor){
+		if(x<0 || y<0 || x>=pixels.length || y>=pixels.length){
+			return false;
+		}
+		if(pixels[x][y] == oldColor){
+			pixels[x][y] = newColor;
+			paintFill(pixels, x-1, y, oldColor, newColor);
+			paintFill(pixels, x+1, y, oldColor, newColor);
+			paintFill(pixels, x, y-1, oldColor, newColor);
+			paintFill(pixels, x, y+1, oldColor, newColor);
+		}
+		return true;
+	}
 }
