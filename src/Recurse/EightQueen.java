44d4e1faf9212b29c0e7968670b6dc4432e074cd
yangboufo <130469Uf>
2013-08-07 21:29:29 +0800

    Famous Eight Queen Problem

--- src/Recurse/EightQueen.java
+++ src/Recurse/EightQueen.java
@@ -1,5 +1,37 @@
 package Recurse;
 
-public class EightQueen {
+import java.util.ArrayList;
 
+//each array is for one matrix, column[row] =c means row r has queen ar c col
+public class EightQueen {
+	public final int grid_size = 8;
+	
+	public void placeQueens(int row, Integer [] columns, ArrayList<Integer[]> result){
+		if(row == grid_size){
+			result.add(columns.clone());
+		}else{
+			for(int col=0; col<grid_size; col++){
+				if(check(columns, row, col)){
+					columns[row] = col;
+					placeQueens(row+1, columns, result);
+				}
+			}
+		}
+	}
+	public boolean check(Integer [] columns, int row, int column){
+		for(int i=0; i<row; i++){
+			int tmp = columns[i];//ith row's queens
+			if(column == tmp){
+				return false;
+			}
+			int rowDiff = Math.abs(i-row);
+			int colDiff = Math.abs(tmp-column);
+			if(rowDiff == colDiff){
+				return false;
+			}
+		}
+		return true;
+		
+		
+	}
 }
